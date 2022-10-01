9d5c3905a88dd781e62e6b76d11756f8ef2c83a5
Partik SIngh <partiksingh@pop-os.localdomain>
2022-10-02 01:44:53 +0530

    Added functionality to add post without uploading an image

--- src/components/ImgUpload.jsx
+++ src/components/ImgUpload.jsx
@@ -14,40 +14,88 @@ function ImgUpload(props) {
     }
   };
   const handleUpload = () => {
-    const uploadTask = storage.ref(`images/${image.name}`).put(image);
-    uploadTask.on(
-      "state_changed",
-      (snapshot) => {
-        // progress function ...
-        setProgress(
-          Math.round((snapshot.bytesTransferred / snapshot.totalBytes) * 100)
-        );
-      },
-      (error) => {
-        // error function ...
-        console.log(error);
-        alert(error.message);
-      },
-      () => {
-        // complete function ...
-        storage
-          .ref("images")
-          .child(image.name)
-          .getDownloadURL()
-          .then((url) => {
-            db.collection("posts").add({
-              timestamp: firebase.firestore.FieldValue.serverTimestamp(),
-              caption: caption,
-              imageUrl: url,
-              username: props.username,
-              avatar: "avatar",
+    if(image){
+      const uploadTask = storage.ref(`images/${image.name}`).put(image);
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          // progress function ...
+          setProgress(
+            Math.round((snapshot.bytesTransferred / snapshot.totalBytes) * 100)
+          );
+        },
+        (error) => {
+          // error function ...
+          console.log(error);
+          alert(error.message);
+        },
+        () => {
+          // complete function ...
+          storage
+            .ref("images")
+            .child(image.name)
+            .getDownloadURL()
+            .then((url) => {
+              db.collection("posts").add({
+                timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+                caption: caption,
+                imageUrl: url,
+                username: props.username,
+                avatar: "avatar",
+              });
+              setProgress(0);
+              setCaption("");
+              setImage(null);
             });
-            setProgress(0);
-            setCaption("");
-            setImage(null);
-          });
-      }
-    );
+        }
+      );
+    }
+    // const uploadTask = storage.ref(`images/${image.name}`).put(image);
+    // uploadTask.on(
+    //   "state_changed",
+    //   (snapshot) => {
+    //     // progress function ...
+    //     setProgress(
+    //       Math.round((snapshot.bytesTransferred / snapshot.totalBytes) * 100)
+    //     );
+    //   },
+    //   (error) => {
+    //     // error function ...
+    //     console.log(error);
+    //     alert(error.message);
+    //   },
+    //   () => {
+    //     // complete function ...
+    //     storage
+    //       .ref("images")
+    //       .child(image.name)
+    //       .getDownloadURL()
+    //       .then((url) => {
+    //         db.collection("posts").add({
+    //           timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+    //           caption: caption,
+    //           imageUrl: url,
+    //           username: props.username,
+    //           avatar: "avatar",
+    //         });
+    //         setProgress(0);
+    //         setCaption("");
+    //         setImage(null);
+    //       });
+    //   }
+    // );
+    else {
+      db.collection("posts").add({
+        timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+        caption: caption,
+        imageUrl: "",
+        username: props.username,
+        avatar: "avatar",
+      })
+      setProgress(0);
+      setCaption("");
+      setImage(null);
+    }
   };
 
   return (
