af457d3a40ea6cfe37d296ca16aa6c8118501767
ansharora28 <ansharorake2@gmail.com>
2022-12-10 03:56:33 +0530

    moved dark mode toggle upwards to prevent overlapping

--- src/script.js
+++ src/script.js
@@ -1,5 +1,5 @@
 const options = {
-  bottom: "32px", // default: '32px'
+  bottom: "48px", // default: '32px'
   right: "32px", // default: '32px'
   left: "unset", // default: 'unset'
   time: "0.3s", // default: '0.3s'
@@ -17,13 +17,11 @@ darkmode.showWidget();
 
 document.getElementById('scrollbar_style').innerHTML = 'body::-webkit-scrollbar { width: 10px; } /* Track */ body::-webkit-scrollbar-track { background: white; } /* Handle */ body::-webkit-scrollbar-thumb { background: linear-gradient( 45deg, #405de6,#5851db,#833ab4,#c13584,#e1306c,#fd1d1d ); border-radius: 10px; }';
 
-document.querySelector("button.darkmode-toggle").addEventListener("click", function() {
-  if (document.body.classList.contains('darkmode--activated'))
-  {
+document.querySelector("button.darkmode-toggle").addEventListener("click", function () {
+  if (document.body.classList.contains('darkmode--activated')) {
     document.getElementById('scrollbar_style').innerHTML = 'body::-webkit-scrollbar { width: 10px; } /* Track */ body::-webkit-scrollbar-track { background: black; } /* Handle */ body::-webkit-scrollbar-thumb { background: linear-gradient( 45deg, #f4f80c, #addb51, #a2b43a, #51c135, #5ce130, #1dfddf ); border-radius: 10px; }';
   }
-  else
-  {
+  else {
     document.getElementById('scrollbar_style').innerHTML = 'body::-webkit-scrollbar { width: 10px; } /* Track */ body::-webkit-scrollbar-track { background: white; } /* Handle */ body::-webkit-scrollbar-thumb { background: linear-gradient( 45deg, #405de6,#5851db,#833ab4,#c13584,#e1306c,#fd1d1d ); border-radius: 10px; }';
   }
 });
