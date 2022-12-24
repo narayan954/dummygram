97626de3d5dfb0f96b5f41cd6e7fcbbbd7e9fd25
Narayan soni <narayansoni854@gmail.com>
2022-12-24 23:01:13 +0530

    fixed formatting

--- src/script.js
+++ src/script.js
@@ -18,11 +18,14 @@ darkmode.showWidget();
 document.getElementById("scrollbar_style").innerHTML =
   "body::-webkit-scrollbar { width: 10px; } /* Track */ body::-webkit-scrollbar-track { background: white; } /* Handle */ body::-webkit-scrollbar-thumb { background: linear-gradient( 45deg, #405de6,#5851db,#833ab4,#c13584,#e1306c,#fd1d1d ); border-radius: 10px; }";
 
-document.querySelector("button.darkmode-toggle").addEventListener("click", function () {
-  if (document.body.classList.contains('darkmode--activated')) {
-    document.getElementById('scrollbar_style').innerHTML = 'body::-webkit-scrollbar { width: 10px; } /* Track */ body::-webkit-scrollbar-track { background: black; } /* Handle */ body::-webkit-scrollbar-thumb { background: linear-gradient( 45deg, #f4f80c, #addb51, #a2b43a, #51c135, #5ce130, #1dfddf ); border-radius: 10px; }';
-  }
-  else {
-    document.getElementById('scrollbar_style').innerHTML = 'body::-webkit-scrollbar { width: 10px; } /* Track */ body::-webkit-scrollbar-track { background: white; } /* Handle */ body::-webkit-scrollbar-thumb { background: linear-gradient( 45deg, #405de6,#5851db,#833ab4,#c13584,#e1306c,#fd1d1d ); border-radius: 10px; }';
-  }
-});
+document
+  .querySelector("button.darkmode-toggle")
+  .addEventListener("click", function () {
+    if (document.body.classList.contains("darkmode--activated")) {
+      document.getElementById("scrollbar_style").innerHTML =
+        "body::-webkit-scrollbar { width: 10px; } /* Track */ body::-webkit-scrollbar-track { background: black; } /* Handle */ body::-webkit-scrollbar-thumb { background: linear-gradient( 45deg, #f4f80c, #addb51, #a2b43a, #51c135, #5ce130, #1dfddf ); border-radius: 10px; }";
+    } else {
+      document.getElementById("scrollbar_style").innerHTML =
+        "body::-webkit-scrollbar { width: 10px; } /* Track */ body::-webkit-scrollbar-track { background: white; } /* Handle */ body::-webkit-scrollbar-thumb { background: linear-gradient( 45deg, #405de6,#5851db,#833ab4,#c13584,#e1306c,#fd1d1d ); border-radius: 10px; }";
+    }
+  });
