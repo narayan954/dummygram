1ca43d3f3ec06ff9efa76dc4c3b0b28b4a56f26e
ansharora28 <ansharorake2@gmail.com>
2022-12-24 12:32:44 +0530

    bring dark mode option to previous position

--- src/script.js
+++ src/script.js
@@ -18,27 +18,11 @@ darkmode.showWidget();
 document.getElementById("scrollbar_style").innerHTML =
   "body::-webkit-scrollbar { width: 10px; } /* Track */ body::-webkit-scrollbar-track { background: white; } /* Handle */ body::-webkit-scrollbar-thumb { background: linear-gradient( 45deg, #405de6,#5851db,#833ab4,#c13584,#e1306c,#fd1d1d ); border-radius: 10px; }";
 
-<<<<<<< HEAD
-document
-  .querySelector("button.darkmode-toggle")
-  .addEventListener("click", function () {
-    if (document.body.classList.contains("darkmode--activated")) {
-      document.getElementById("scrollbar_style").innerHTML =
-        "body::-webkit-scrollbar { width: 10px; } /* Track */ body::-webkit-scrollbar-track { background: black; } /* Handle */ body::-webkit-scrollbar-thumb { background: linear-gradient( 45deg, #f4f80c, #addb51, #a2b43a, #51c135, #5ce130, #1dfddf ); border-radius: 10px; }";
-    } else {
-      document.getElementById("scrollbar_style").innerHTML =
-        "body::-webkit-scrollbar { width: 10px; } /* Track */ body::-webkit-scrollbar-track { background: white; } /* Handle */ body::-webkit-scrollbar-thumb { background: linear-gradient( 45deg, #405de6,#5851db,#833ab4,#c13584,#e1306c,#fd1d1d ); border-radius: 10px; }";
-    }
-  });
-=======
-document.querySelector("button.darkmode-toggle").addEventListener("click", function() {
-  if (document.body.classList.contains('darkmode--activated'))
-  {
+document.querySelector("button.darkmode-toggle").addEventListener("click", function () {
+  if (document.body.classList.contains('darkmode--activated')) {
     document.getElementById('scrollbar_style').innerHTML = 'body::-webkit-scrollbar { width: 10px; } /* Track */ body::-webkit-scrollbar-track { background: black; } /* Handle */ body::-webkit-scrollbar-thumb { background: linear-gradient( 45deg, #f4f80c, #addb51, #a2b43a, #51c135, #5ce130, #1dfddf ); border-radius: 10px; }';
   }
-  else
-  {
+  else {
     document.getElementById('scrollbar_style').innerHTML = 'body::-webkit-scrollbar { width: 10px; } /* Track */ body::-webkit-scrollbar-track { background: white; } /* Handle */ body::-webkit-scrollbar-thumb { background: linear-gradient( 45deg, #405de6,#5851db,#833ab4,#c13584,#e1306c,#fd1d1d ); border-radius: 10px; }';
   }
 });
->>>>>>> parent of af457d3 (moved dark mode toggle upwards to prevent overlapping)
