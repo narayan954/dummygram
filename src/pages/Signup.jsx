f207f042f1f47ccea5255f528f5744f6144cec1b
Vineet-Singh-Chauhan <chauhansvineet@gmail.com>
2023-03-10 20:52:49 +0530

    sign up done conflicts solved

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { getModalStyle, useStyles } from "../App";
 import { updateProfile } from "firebase/auth";
-import { updateProfile } from "firebase/auth";
 import {
   auth,
   storage,
