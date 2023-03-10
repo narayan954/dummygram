02a74d5b20af6d64e7e4e863695fa914421c357f
Tanmay Sarkar <sarkartanmay393@gmail.com>
2023-03-10 11:43:21 +0530

    hotfix: Share Functionality

--- src/App.jsx
+++ src/App.jsx
@@ -62,7 +62,9 @@ function App() {
   const [loadMorePosts, setLoadMorePosts] = useState(false);
   const [openNewUpload, setOpenNewUpload] = useState(false);
   const [logout, setLogout] = useState(false);
-  const [openShareModal, setOpenShareModal] = useState(true);
+  const [openShareModal, setOpenShareModal] = useState(false);
+  const [currentPostLink, setCurrentPostLink] = useState("");
+  const [postText, setPostText] = useState("");
 
   const buttonStyle = {
     background: "linear-gradient(40deg, #e107c1, #59afc7)",
@@ -267,7 +269,9 @@ function App() {
           </p>
 
           <div className="mx-2">
-            <IconButton onClick={() => alert("Twit")}>
+            <IconButton
+              href={`whatsapp://send/?text=${postText}%20${currentPostLink}`}
+            >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 x="0px"
@@ -279,7 +283,9 @@ function App() {
                 <path d="M 15 3 C 8.373 3 3 8.373 3 15 C 3 17.251208 3.6323415 19.350068 4.7109375 21.150391 L 3.1074219 27 L 9.0820312 25.431641 C 10.829354 26.425062 12.84649 27 15 27 C 21.627 27 27 21.627 27 15 C 27 8.373 21.627 3 15 3 z M 10.892578 9.4023438 C 11.087578 9.4023438 11.287937 9.4011562 11.460938 9.4101562 C 11.674938 9.4151563 11.907859 9.4308281 12.130859 9.9238281 C 12.395859 10.509828 12.972875 11.979906 13.046875 12.128906 C 13.120875 12.277906 13.173313 12.453437 13.070312 12.648438 C 12.972312 12.848437 12.921344 12.969484 12.777344 13.146484 C 12.628344 13.318484 12.465078 13.532109 12.330078 13.662109 C 12.181078 13.811109 12.027219 13.974484 12.199219 14.271484 C 12.371219 14.568484 12.968563 15.542125 13.851562 16.328125 C 14.986562 17.342125 15.944188 17.653734 16.242188 17.802734 C 16.540187 17.951734 16.712766 17.928516 16.884766 17.728516 C 17.061766 17.533516 17.628125 16.864406 17.828125 16.566406 C 18.023125 16.268406 18.222188 16.319969 18.492188 16.417969 C 18.766188 16.515969 20.227391 17.235766 20.525391 17.384766 C 20.823391 17.533766 21.01875 17.607516 21.09375 17.728516 C 21.17075 17.853516 21.170828 18.448578 20.923828 19.142578 C 20.676828 19.835578 19.463922 20.505734 18.919922 20.552734 C 18.370922 20.603734 17.858562 20.7995 15.351562 19.8125 C 12.327563 18.6215 10.420484 15.524219 10.271484 15.324219 C 10.122484 15.129219 9.0605469 13.713906 9.0605469 12.253906 C 9.0605469 10.788906 9.8286563 10.071437 10.097656 9.7734375 C 10.371656 9.4754375 10.692578 9.4023438 10.892578 9.4023438 z"></path>
               </svg>
             </IconButton>
-            <IconButton onClick={() => alert("Twit")}>
+            <IconButton
+              href={`https://www.linkedin.com/sharing/share-offsite/?url=${currentPostLink}`}
+            >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 x="0px"
@@ -291,7 +297,9 @@ function App() {
                 <path d="M15,3C8.373,3,3,8.373,3,15c0,6.627,5.373,12,12,12s12-5.373,12-12C27,8.373,21.627,3,15,3z M10.496,8.403 c0.842,0,1.403,0.561,1.403,1.309c0,0.748-0.561,1.309-1.496,1.309C9.561,11.022,9,10.46,9,9.712C9,8.964,9.561,8.403,10.496,8.403z M12,20H9v-8h3V20z M22,20h-2.824v-4.372c0-1.209-0.753-1.488-1.035-1.488s-1.224,0.186-1.224,1.488c0,0.186,0,4.372,0,4.372H14v-8 h2.918v1.116C17.294,12.465,18.047,12,19.459,12C20.871,12,22,13.116,22,15.628V20z"></path>
               </svg>
             </IconButton>
-            <IconButton onClick={() => alert("Twit")}>
+            <IconButton
+              href={`https://telegram.me/share/url?url=${currentPostLink}&text=${postText}`}
+            >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 x="0px"
@@ -308,13 +316,18 @@ function App() {
           <Input
             readOnly
             autoFocus
-            value={"currentPostLink"}
+            disableUnderline
+            value={currentPostLink}
             onClick={() => {
-              window.navigator.clipboard.writeText("");
+              window.navigator.clipboard.writeText(currentPostLink);
+              enqueueSnackbar(`Copied Post Link!`, {
+                variant: "success",
+              });
             }}
             sx={{
               marginY: "8px",
               width: "80%",
+              padding: "1rem",
             }}
           />
         </div>
@@ -438,7 +451,15 @@ function App() {
                   ) : (
                     <div className="app__posts">
                       {posts.map(({ id, post }) => (
-                        <Post key={id} postId={id} user={user} post={post} />
+                        <Post
+                          key={id}
+                          postId={id}
+                          user={user}
+                          post={post}
+                          shareModal={setOpenShareModal}
+                          setLink={setCurrentPostLink}
+                          setPostText={setPostText}
+                        />
                       ))}
                     </div>
                   )}
