109ef3156e702766688f1546ad3c9cbe16803336
Lakshya Singh <lakshyaasin@gmail.com>
2023-05-25 13:14:11 +0530

    navigation simpler

--- src/App.jsx
+++ src/App.jsx
@@ -197,13 +197,10 @@ function App() {
           alt="dummygram"
           className="app__header__img w-100"
           onClick={() => {
-            if(location.pathname==='/dummygram/login'){
-              navigate("/dummygram/login"); 
-            }
-            else if (location.pathname === '/dummygram/signup'){
-              navigate("/dummygram/signup");               
-            }
-            else{
+            if (
+              location.pathname !== "/dummygram/login" &&
+              location.pathname !== "/dummygram/signup"
+            ) {
               navigate("/dummygram/");
             }
             window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
