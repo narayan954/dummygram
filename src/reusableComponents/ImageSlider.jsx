fd5b505700768450bf72529ef91d04f8b59b2a0c
Soulpurr <92094765+Soulpurr@users.noreply.github.com>
2023-06-04 11:27:47 +0530

    #414-card items in row

--- src/reusableComponents/ImageSlider.jsx
+++ src/reusableComponents/ImageSlider.jsx
@@ -43,7 +43,7 @@ const ImageSlider = ({ slides, isCommentBox }) => {
             delayTime={1000}
             style={{
               width: "100%",
-              height: "100%",
+              height: "14rem",
               objectFit: "contain",
             }}
           />
