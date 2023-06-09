60396129534c99b33ec2049ef646be497c5a4dfc
akhilesh <akhileshkumar@gofynd.com>
2023-06-09 08:21:42 +0530

    Improving

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,11 +1,17 @@
 import React, { useState } from 'react';
 import emailjs from 'emailjs-com';
+import Modal from "react-modal";
 
 const Feedback = () => {
     const [feedback, setFeedback] = useState('')
     const [submitted, setSubmitted] = useState(false)
     const [email, setEmail] = useState('')
-    emailjs.init('1AkXF-PC-6FG2Ym15')
+    const emailjsKey = process.env.VITE_DUMMYGRAM_EMAILJS_KEY;
+    emailjs.init(emailjsKey);
+    const [openModal, setopenModal] = useState(false);
+    const open = () => {
+        setopenModal((currentValue) => !currentValue);
+    };
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -25,36 +31,38 @@ const Feedback = () => {
             }).catch((err) => {
                 console.log('Error sending feedback:', error)
             })
-
-
-
-
     };
+return (
+        <div>
+            <Modal
+                isOpen={props.showModal}
+                ariaHideApp={false}>
+                <Feedback showModal={openModal} toggalProfileModal={open} />
+                <button className="btn  mt-3" onClick={open}></button>
+                <form onSubmit={handleSubmit} >
+                    <textarea placeholder="enter your feedback"
+                        style={{ border: '2px solid #ccc', boxSizing: 'border-box', borderRadius: '4px', resize: 'none', width: '50%', height: '100px' }}
+                        value={feedback}
+                        onChange={(e) => setFeedback(e.target.value)}>
 
-    <div>
-        <form onSubmit={handleSubmit} >
-            <textarea placeholder="enter your feedback"
-                style={{ border: '2px solid #ccc', boxSizing: 'border-box', borderRadius: '4px', resize: 'none', width: '50%', height: '100px' }}
-                value={feedback}
-                onChange={(e) => setFeedback(e.target.value)}>
+                    </textarea>
+                    <input
+                        name="email"
+                        style={{ display: 'flex', alignItems: 'right', border: '2px solid #ccc', width: '50%', height: '50px', borderRadius: '4px' }}
+                        placeholder='enter your email'
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
 
-            </textarea>
-            <input
-                name="email"
-                style={{ display: 'flex', alignItems: 'right', border: '2px solid #ccc', width: '50%', height: '50px', borderRadius: '4px' }}
-                placeholder='enter your email'
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-            />
-
-            <button type="submit" onClick={handleSubmit} style={{ borderRadius: '4px', border: '1px solid #ccc', height: '40px', width: '100px', marginTop: '30px' }}>Submit</button>
-        </form>
-        {submitted && (
-            <div>
-                <p>Sent!</p>
-            </div>
-        )}
-    </div>
-
+                    <button type="submit" onClick={handleSubmit} style={{ borderRadius: '4px', border: '1px solid #ccc', height: '40px', width: '100px', marginTop: '30px' }}>Submit</button>
+                </form>
+                {submitted && (
+                    <div>
+                        <p>Sent!</p>
+                    </div>
+                )}
+            </Modal>
+        </div>
+    );
 }
 export default Feedback;
