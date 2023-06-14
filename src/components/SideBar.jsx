f468dbda463590a2ec94dd8a2f7acb5870251566
Gurmeet Singh <83361657+Gurmeet-Singh657@users.noreply.github.com>
2023-06-15 02:17:35 +0530

    Post Uploading Criteria Changed Completely (#452)
    * Post Uploading Criteria Changed Completely
    
    * format code
    
    * NewPost Shifted to Sidebar & getting authorized user from firebase
    
    * fixed formatting
    
    * removed unused imports
    
    ---------
    
    Co-authored-by: Narayan soni <narayansoni854@gmail.com>
    Co-authored-by: Narayan soni <narayan.soni@cosmocloud.io>

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -5,13 +5,19 @@ import { Link, useNavigate } from "react-router-dom";
 
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
+import { AiOutlineClose } from "react-icons/ai";
+import { Dialog } from "@mui/material";
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
-// icons import
 import HomeIcon from "@mui/icons-material/Home";
+import ImgUpload from "../components/ImgUpload";
 import React from "react";
+import { auth } from "../lib/firebase";
+import { useState } from "react";
 
 function SideBar(props) {
   const navigate = useNavigate();
+  const [openNewUpload, setOpenNewUpload] = useState(false);
+  const user = auth.currentUser;
 
   return (
     <div className="sidebar">
@@ -21,13 +27,13 @@ function SideBar(props) {
             <HomeIcon className="icon" /> <span>Home</span>
           </Link>
         </li>
-        <li>
-          <Link to="/dummygram">
+        <li onClick={() => setOpenNewUpload(true)}>
+          <div className="sidebar_align">
             <AddCircleOutlineIcon className="icon" /> <span>New Post</span>
-          </Link>
+          </div>
         </li>
         <li onClick={() => navigate("/dummygram/favourites")}>
-          <div className="profile">
+          <div className="sidebar_align">
             <FavoriteBorderIcon className="icon" /> <span>Favourites</span>
           </div>
         </li>
@@ -42,11 +48,58 @@ function SideBar(props) {
             })
           }
         >
-          <div className="profile">
+          <div className="sidebar_align">
             <AccountCircleIcon className="icon" /> <span>Profile</span>
           </div>
         </li>
       </ul>
+      <Dialog
+        PaperProps={{
+          sx: {
+            width: "60vw",
+            height: "60vh",
+          },
+        }}
+        open={openNewUpload}
+        onClose={() => setOpenNewUpload(false)}
+      >
+        <div
+          style={{
+            backgroundColor: "var(--bg-color)",
+            textAlign: "center",
+            color: "var(--color)",
+          }}
+        >
+          <AiOutlineClose
+            onClick={() => {
+              setOpenNewUpload(false);
+            }}
+            size={18}
+            style={{
+              position: "absolute",
+              right: "1rem",
+              top: "1rem",
+              cursor: "pointer",
+            }}
+          />
+          <p
+            style={{
+              fontSize: "17px",
+              fontWeight: 500,
+              color: "var(--color)",
+              marginTop: "10px",
+              marginBottom: "8px",
+            }}
+          >
+            Create new post
+          </p>
+          <hr />
+          <ImgUpload
+            user={user}
+            onUploadComplete={() => setOpenNewUpload(false)}
+          />
+        </div>
+      </Dialog>
     </div>
   );
 }
