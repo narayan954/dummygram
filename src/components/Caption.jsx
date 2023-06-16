594ccb79d0affe0deb6ab0d267dc676f973310c1
Narayan soni <narayan.soni@cosmocloud.io>
2023-06-17 04:46:35 +0530

    format code

--- src/components/Caption.jsx
+++ src/components/Caption.jsx
@@ -4,37 +4,36 @@
  * @returns {JSX.Element}
  * @constructor
  */
-export default function Caption({caption}) {
+export default function Caption({ caption }) {
+  const urlRegex = /(https?:\/\/\S+(?<![\s.,!?]))/g;
 
-    const urlRegex = /(https?:\/\/\S+(?<![\s.,!?]))/g;
+  // Split the caption into parts, separating URLs
+  const parts = caption.split(urlRegex);
 
-    // Split the caption into parts, separating URLs
-    const parts = caption.split(urlRegex);
+  // Function to check if a part URL
+  /**
+   *
+   * @param part Array
+   * @returns {boolean}
+   */
+  const isUrl = (part) => urlRegex.test(part);
 
-    // Function to check if a part URL
-    /**
-     *
-     * @param part Array
-     * @returns {boolean}
-     */
-    const isUrl = (part) => urlRegex.test(part);
+  // Render each part of the caption
+  const renderCaptionParts = () => {
+    return parts.map((part, index) => {
+      if (isUrl(part)) {
+        // If the part is a URL, render it as a clickable link.
+        return (
+          <a key={index} href={part} target="_blank" rel="noopener noreferrer">
+            {part}
+          </a>
+        );
+      } else {
+        // If the part is not a URL, render it as plain text.
+        return <span key={index}>{part}</span>;
+      }
+    });
+  };
 
-    // Render each part of the caption
-    const renderCaptionParts = () => {
-        return parts.map((part, index) => {
-            if (isUrl(part)) {
-                // If the part is a URL, render it as a clickable link.
-                return (
-                    <a key={index} href={part} target="_blank" rel="noopener noreferrer">
-                        {part}
-                    </a>
-                );
-            } else {
-                // If the part is not a URL, render it as plain text.
-                return <span key={index}>{part}</span>;
-            }
-        });
-    };
-
-    return <>{renderCaptionParts()}</>;
+  return <>{renderCaptionParts()}</>;
 }
--- src/components/ReadMore.jsx
+++ src/components/ReadMore.jsx
@@ -1,4 +1,5 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
+
 import Caption from "./Caption.jsx";
 
 /**
@@ -7,59 +8,60 @@ import Caption from "./Caption.jsx";
  * @returns {JSX.Element}
  * @constructor
  */
-function ReadMore({caption}) {
-    const [desc, setDesc] = useState("");
-    const [size, setSize] = useState("");
-    const [expand, setExpand] = useState(false);
+export default function ReadMore({ caption }) {
+  const [desc, setDesc] = useState("");
+  const [size, setSize] = useState("");
+  const [expand, setExpand] = useState(false);
 
-    const handleReadMore = (e) => {
-        e.preventDefault();
-        setExpand(true);
-        setDesc(caption);
-    };
+  const handleReadMore = (e) => {
+    e.preventDefault();
+    setExpand(true);
+    setDesc(caption);
+  };
 
-    const handleReadLess = (e) => {
-        e.preventDefault();
-        setExpand(false);
-        setDesc(caption.split(" ").splice(0, 14).join(" "));
-    };
+  const handleReadLess = (e) => {
+    e.preventDefault();
+    setExpand(false);
+    setDesc(caption.split(" ").splice(0, 14).join(" "));
+  };
 
-    useEffect(() => {
-        const splitDesc = caption.split(" ");
-        const sz = splitDesc.length;
-        setSize(sz);
-        if (sz > 14) {
-            setDesc(splitDesc.splice(0, 14).join(" "));
-        } else {
-            setDesc(caption);
-        }
-    }, [caption]);
+  useEffect(() => {
+    const splitDesc = caption.split(" ");
+    const sz = splitDesc.length;
+    setSize(sz);
+    if (sz > 14) {
+      setDesc(splitDesc.splice(0, 14).join(" "));
+    } else {
+      setDesc(caption);
+    }
+  }, [caption]);
 
-    return (
+  return (
+    <>
+      {size > 14 ? (
         <>
-            {size > 14 ? (
-                <>
-                    {expand === false ? (
-                        <>
-                            <Caption caption={desc}/>
-                            ...
-                            <span className="read__more__less" onClick={handleReadMore}>Read More</span>
-                        </>
-                    ) : (
-                        <>
-
-                            <Caption caption={caption}/>
-                            <span className="read__more__less" onClick={handleReadLess}>Read Less</span>
-                        </>
-                    )}
-                </>
-            ) : (
-                <>
-                    <Caption caption={caption}/>
-                </>
-            )}
+          {expand === false ? (
+            <>
+              <Caption caption={desc} />
+              ...
+              <span className="read__more__less" onClick={handleReadMore}>
+                Read More
+              </span>
+            </>
+          ) : (
+            <>
+              <Caption caption={caption} />
+              <span className="read__more__less" onClick={handleReadLess}>
+                Read Less
+              </span>
+            </>
+          )}
         </>
-    );
+      ) : (
+        <>
+          <Caption caption={caption} />
+        </>
+      )}
+    </>
+  );
 }
-
-export default ReadMore;
