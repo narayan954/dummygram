032235689b3f053b10ffeff8da0c2ac6257ca991
Gurmeet <gurmeetsingh222003@gmail.com>
2023-06-17 16:51:58 +0530

    Responsiveness for Smallest Android Phones made responsives

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -5,52 +5,38 @@ import React from "react";
 import { auth } from "../lib/firebase";
 
 export default function NewPost({ openNewUpload, setOpenNewUpload }) {
-  const user = auth.currentUser;
-  console.log("I am Happy Today");
-  return (
-    <Dialog
-      PaperProps={{
-        className: "dialogStyle",
-      }}
-      open={openNewUpload}
-      onClose={() => setOpenNewUpload(false)}
-    >
-      <div
-        style={{
-          backgroundColor: "var(--bg-color)",
-          textAlign: "center",
-          color: "var(--color)",
-        }}
-      >
-        <AiOutlineClose
-          onClick={() => {
-            setOpenNewUpload(false);
-          }}
-          size={18}
-          style={{
-            position: "absolute",
-            right: "1rem",
-            top: "1rem",
-            cursor: "pointer",
-          }}
-        />
-        <p
-          style={{
-            fontSize: "17px",
-            fontWeight: 500,
-            color: "var(--color)",
-            marginTop: "10px",
-            marginBottom: "8px",
-          }}
+    const user = auth.currentUser;
+    return (
+        <Dialog
+            PaperProps={{
+                className: "dialogStyle",
+            }}
+            open={openNewUpload}
+            onClose={() => setOpenNewUpload(false)}
         >
-          Create new post
-        </p>
-        <hr />
-        <ImgUpload
-          user={user}
-          onUploadComplete={() => setOpenNewUpload(false)}
-        />
-      </div>
-    </Dialog>
-  );
+            <div
+                style={{
+                    backgroundColor: "var(--bg-color)",
+                    textAlign: "center",
+                    color: "var(--color)",
+                }}
+            >
+                <AiOutlineClose
+                    onClick={() => {
+                        setOpenNewUpload(false);
+                    }}
+                    size={"1rem"}
+                    className="crossIcon"
+                />
+                <p className="createNewPost">
+                    Create new post
+                </p>
+                <hr />
+                <ImgUpload
+                    user={user}
+                    onUploadComplete={() => setOpenNewUpload(false)}
+                />
+            </div>
+        </Dialog>
+    );
 }
