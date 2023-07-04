8c055bb91a81b245342e133b3d61c21c12f0ff62
Palak <Palakgoyal0304@gmail.com>
2023-07-04 16:48:03 +0530

    Create guidelines page for dummygram (#644)
    * Create guidelines page for dummygram
    
    * formatting
    
    ---------
    
    Co-authored-by: Narayan soni <narayansoni854@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./index.css";
 import {
   About,
   Feedback,
+  Guidelines,
   LoginScreen,
   PostView,
   Profile,
@@ -301,6 +302,8 @@ function App() {
           <Route path="/dummygram/about" element={<About />} />
 
           <Route path="/dummygram/feedback" element={<Feedback />} />
+
+          <Route path="/dummygram/guidelines" element={<Guidelines />} />
 
           <Route path="/dummygram/login" element={<LoginScreen />} />
 
