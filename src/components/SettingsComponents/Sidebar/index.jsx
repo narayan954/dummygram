caa7ec4c0224a2cf42e0d70c87074baf6838364d
Narayan soni <narayansoni854@gmail.com>
2023-07-07 23:06:06 +0530

    code formatting

--- src/components/SettingsComponents/Sidebar/index.jsx
+++ src/components/SettingsComponents/Sidebar/index.jsx
@@ -1,21 +1,22 @@
 import "./index.css";
+
 import { Link, Outlet } from "react-router-dom";
-import VolumeUpOutlinedIcon from '@mui/icons-material/VolumeUpOutlined';
-import NotificationsIcon from '@mui/icons-material/Notifications';
-import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
-import ContactSupportOutlinedIcon from '@mui/icons-material/ContactSupportOutlined';
-import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
-import SecurityOutlinedIcon from '@mui/icons-material/SecurityOutlined';
-import ManageAccountsOutlinedIcon from '@mui/icons-material/ManageAccountsOutlined';
+
+import ContactSupportOutlinedIcon from "@mui/icons-material/ContactSupportOutlined";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import InfoOutlinedIcon from "@mui/icons-material/InfoOutlined";
+import ManageAccountsOutlinedIcon from "@mui/icons-material/ManageAccountsOutlined";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import SecurityOutlinedIcon from "@mui/icons-material/SecurityOutlined";
+import VolumeUpOutlinedIcon from "@mui/icons-material/VolumeUpOutlined";
 
 const SettingsSidebar = () => {
   return (
-    <div >
+    <div>
       <div className="settings-sidebar-container">
         <ul className="settings-sidebar-sub-container">
           <li>
-            <Link
-              to="/dummygram/settings/sounds">
+            <Link to="/dummygram/settings/sounds">
               <VolumeUpOutlinedIcon className="icon" /> <span>Sound</span>
             </Link>
           </li>
@@ -24,7 +25,7 @@ const SettingsSidebar = () => {
               className="settings-sidebar-item"
               to="/dummygram/settings/notification"
             >
-              <NotificationsIcon className="icon" />  <span>Notification</span>
+              <NotificationsIcon className="icon" /> <span>Notification</span>
             </Link>
           </li>
           <li>
@@ -40,14 +41,17 @@ const SettingsSidebar = () => {
               className="settings-sidebar-item"
               to="/dummygram/settings/theme"
             >
-              <DarkModeOutlinedIcon className="icon" /><span>Theme</span>
-            </Link></li>
+              <DarkModeOutlinedIcon className="icon" />
+              <span>Theme</span>
+            </Link>
+          </li>
           <li>
             <Link
               className="settings-sidebar-item"
               to="/dummygram/settings/lorem"
             >
-              <SecurityOutlinedIcon className="icon" /><span>Privacy and Security</span>
+              <SecurityOutlinedIcon className="icon" />
+              <span>Privacy and Security</span>
             </Link>
           </li>
           <li>
@@ -55,7 +59,7 @@ const SettingsSidebar = () => {
               className="settings-sidebar-item"
               to="/dummygram/settings/ipsum"
             >
-              < ContactSupportOutlinedIcon className="icon" /> <span> Help</span>
+              <ContactSupportOutlinedIcon className="icon" /> <span> Help</span>
             </Link>
           </li>
           <li>
@@ -63,7 +67,8 @@ const SettingsSidebar = () => {
               className="settings-sidebar-item"
               to="/dummygram/settings/account"
             >
-              <ManageAccountsOutlinedIcon className="icon" /> <span>Account</span>
+              <ManageAccountsOutlinedIcon className="icon" />{" "}
+              <span>Account</span>
             </Link>
           </li>
         </ul>
