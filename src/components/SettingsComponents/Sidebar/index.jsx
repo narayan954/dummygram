e37b896d55c3cec6a87df0b0c931cb0fb584a7da
Bharti Vilas Nagpure <104631882+BhartiNagpure@users.noreply.github.com>
2023-07-07 12:40:35 +0530

    setting page sidebar icon

--- src/components/SettingsComponents/Sidebar/index.jsx
+++ src/components/SettingsComponents/Sidebar/index.jsx
@@ -1,60 +1,75 @@
 import "./index.css";
-
 import { Link, Outlet } from "react-router-dom";
-
-import React from "react";
+import VolumeUpOutlinedIcon from '@mui/icons-material/VolumeUpOutlined';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
+import ContactSupportOutlinedIcon from '@mui/icons-material/ContactSupportOutlined';
+import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
+import SecurityOutlinedIcon from '@mui/icons-material/SecurityOutlined';
+import ManageAccountsOutlinedIcon from '@mui/icons-material/ManageAccountsOutlined';
 
 const SettingsSidebar = () => {
   return (
-    <>
+    <div >
       <div className="settings-sidebar-container">
         <ul className="settings-sidebar-sub-container">
-          <Link
-            className="settings-sidebar-item"
-            to="/dummygram/settings/sounds"
-          >
-            Sounds
-          </Link>
-          <Link
-            className="settings-sidebar-item"
-            to="/dummygram/settings/notificatin"
-          >
-            Notification
-          </Link>
-          <Link
-            className="settings-sidebar-item"
-            to="/dummygram/settings/about"
-          >
-            About
-          </Link>
-          <Link
-            className="settings-sidebar-item"
-            to="/dummygram/settings/theme"
-          >
-            Theme
-          </Link>
-          <Link
-            className="settings-sidebar-item"
-            to="/dummygram/settings/guidelines"
-          >
-            Privacy and Security
-          </Link>
-          <Link
-            className="settings-sidebar-item"
-            to="/dummygram/settings/ipsum"
-          >
-            Help
-          </Link>
-          <Link
-            className="settings-sidebar-item"
-            to="/dummygram/settings/dolor"
-          >
-            dolor
-          </Link>
+          <li>
+            <Link
+              to="/dummygram/settings/sounds">
+              <VolumeUpOutlinedIcon className="icon" /> <span>Sound</span>
+            </Link>
+          </li>
+          <li>
+            <Link
+              className="settings-sidebar-item"
+              to="/dummygram/settings/notification"
+            >
+              <NotificationsIcon className="icon" />  <span>Notification</span>
+            </Link>
+          </li>
+          <li>
+            <Link
+              className="settings-sidebar-item"
+              to="/dummygram/settings/about"
+            >
+              <InfoOutlinedIcon className="icon" /> <span>About</span>
+            </Link>
+          </li>
+          <li>
+            <Link
+              className="settings-sidebar-item"
+              to="/dummygram/settings/theme"
+            >
+              <DarkModeOutlinedIcon className="icon" /><span>Theme</span>
+            </Link></li>
+          <li>
+            <Link
+              className="settings-sidebar-item"
+              to="/dummygram/settings/lorem"
+            >
+              <SecurityOutlinedIcon className="icon" /><span>Privacy and Security</span>
+            </Link>
+          </li>
+          <li>
+            <Link
+              className="settings-sidebar-item"
+              to="/dummygram/settings/ipsum"
+            >
+              < ContactSupportOutlinedIcon className="icon" /> <span> Help</span>
+            </Link>
+          </li>
+          <li>
+            <Link
+              className="settings-sidebar-item"
+              to="/dummygram/settings/account"
+            >
+              <ManageAccountsOutlinedIcon className="icon" /> <span>Account</span>
+            </Link>
+          </li>
         </ul>
       </div>
       <Outlet />
-    </>
+    </div>
   );
 };
 
