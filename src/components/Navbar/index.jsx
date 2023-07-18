aa134e05c5a9beb340ac6e7f9a515ac19fd4c042
narayan soni <narayansoni854@gmail.com>
2023-07-18 17:12:05 +0530

    fixed errors and extra code

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -3,10 +3,10 @@ import "./index.css";
 import React, { useEffect } from "react";
 
 import { AiOutlineInsertRowAbove } from "react-icons/ai";
+import { Logo } from "../../reusableComponents";
 import { auth } from "../../lib/firebase";
 import { blue } from "@mui/material/colors";
 import { useNavigate } from "react-router-dom";
-import { Logo } from "../../reusableComponents"
 
 function Navbar({ onClick, user, setUser }) {
   const navigate = useNavigate();
@@ -30,46 +30,12 @@ function Navbar({ onClick, user, setUser }) {
     location.pathname !== "/dummygram/signup" && (
       <div className="app__header">
         <Logo />
-
-        {
-          user && (
-            <>
-              <div className="container">
-                <div className="rowConvert" onClick={onClick}>
-                  <AiOutlineInsertRowAbove style={{ margin: "auto" }} size={30} />
-                </div>
-                <ClickAwayListener onClickAway={() => setOpen(false)}>
-                  <Button
-                    onClick={() => setOpen((cur) => !cur)}
-                    color="secondary"
-                    variant="contained"
-                    className="button-style"
-                  >
-                    <FaUserCircle fontSize="large" />
-                    {open && (
-                      <Box className="nav-menu">
-                        <Box
-                          className="nav-menu-item"
-                          onClick={() => navigate("/dummygram/settings")}
-                        >
-                          <Typography fontSize="0.9rem">Settings</Typography>
-                        </Box>
-                        <Divider />
-                        <Box
-                          className="nav-menu-item"
-                          onClick={() => setLogout(true)}
-                        >
-                          <Typography fontSize="0.9rem">Log Out</Typography>
-                        </Box>
-                      </Box>
-                    )}
-                  </Button>
-                </ClickAwayListener>
-              </div>
-            </>
-          )
-        }
-      </div >
+        <div className="container">
+          <div className="rowConvert" onClick={onClick}>
+            <AiOutlineInsertRowAbove style={{ margin: "auto" }} size={30} />
+          </div>
+        </div>
+      </div>
     )
   );
 }
