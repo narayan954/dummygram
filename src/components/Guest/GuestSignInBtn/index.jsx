64cf45ea4ea2392a6f53d036746986dd7a43d12c
narayan soni <narayansoni854@gmail.com>
2023-07-20 21:23:32 +0530

    add icon and change text color for signin as guest

---
+++ src/components/Guest/GuestSignInBtn/index.jsx
@@ -0,0 +1,44 @@
+import "./index.css";
+
+import { playErrorSound, playSuccessSound } from "../../../js/sounds";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { auth } from "../../../lib/firebase";
+import { faRedditAlien } from "@fortawesome/free-brands-svg-icons";
+import { useNavigate } from "react-router-dom";
+import { useSnackbar } from "notistack";
+
+const GuestSignInBtn = ({ show }) => {
+  const navigate = useNavigate();
+  const { enqueueSnackbar } = useSnackbar();
+
+  function guestSignIn() {
+    auth
+      .signInAnonymously()
+      .then(() => {
+        playSuccessSound();
+        enqueueSnackbar("Guest Sign In successfull", {
+          variant: "success",
+        });
+        navigate("/dummygram/");
+      })
+      .catch((error) => {
+        playErrorSound();
+        enqueueSnackbar(error.message, {
+          variant: "error",
+        });
+      });
+  }
+
+  return (
+    <button
+      onClick={guestSignIn}
+      className="guest-sign-in-button other__login"
+      style={{ display: show ? "inline-block" : "none" }}
+    >
+      <FontAwesomeIcon icon={faRedditAlien} /> Sign up as a Guest!
+    </button>
+  );
+};
+
+export default GuestSignInBtn;
