f211d74f0fad4fda290bd23041f193bdf3773b91
Anamika Yadav <aanyayadav419@gmail.com>
2023-07-27 00:56:45 +0530

    removed comments

--- src/components/SideBar/Footer.jsx
+++ src/components/SideBar/Footer.jsx
@@ -1,9 +1,9 @@
 import GitHubIcon from "@mui/icons-material/GitHub";
-import InfoIcon from '@mui/icons-material/Info';
-import FeedbackIcon from '@mui/icons-material/Feedback';
-import HelpIcon from '@mui/icons-material/Help';
-import ContributorsIcon from '@mui/icons-material/Diversity3';
-import GuidelinesIcon from '@mui/icons-material/LibraryBooks';
+import InfoIcon from "@mui/icons-material/Info";
+import FeedbackIcon from "@mui/icons-material/Feedback";
+import HelpIcon from "@mui/icons-material/Help";
+import ContributorsIcon from "@mui/icons-material/Diversity3";
+import GuidelinesIcon from "@mui/icons-material/LibraryBooks";
 import { Link } from "react-router-dom";
 
 const Footer = () => {
@@ -12,32 +12,54 @@ const Footer = () => {
       <ul className="sidebar-footer-container">
         <li>
           <a href="https://github.com/narayan954/dummygram" target="_blank">
-            <span style={{display: "flex", gap: "6px"}}><GitHubIcon className="footer-icons" fontSize="small" /> Repository</span>
+            <span style={{ display: "flex", gap: "6px" }}>
+              <GitHubIcon className="footer-icons" fontSize="small" />{" "}
+              Repository
+            </span>
           </a>
         </li>
         <li>
           <Link to="/dummygram/about" className="footer-link">
-            <span style={{display: "flex", gap: "6px"}}><InfoIcon className="footer-icons" fontSize="small" />About</span>
+            <span style={{ display: "flex", gap: "6px" }}>
+              <InfoIcon className="footer-icons" fontSize="small" />
+              About
+            </span>
           </Link>
         </li>
-        <li >
-          <Link style={{display:"flex"}} to="/dummygram/feedback" className="footer-link">
-            <span style={{display: "flex", gap: "6px"}}><FeedbackIcon className="footer-icons" fontSize="small" />Feedback</span>
+        <li>
+          <Link
+            style={{ display: "flex" }}
+            to="/dummygram/feedback"
+            className="footer-link"
+          >
+            <span style={{ display: "flex", gap: "6px" }}>
+              <FeedbackIcon className="footer-icons" fontSize="small" />
+              Feedback
+            </span>
           </Link>
         </li>
         <li>
           <Link to="/dummygram/contributors" className="footer-link">
-            <span style={{display: "flex", gap: "6px"}}><ContributorsIcon className="footer-icons" fontSize="small" />Contributors</span>
+            <span style={{ display: "flex", gap: "6px" }}>
+              <ContributorsIcon className="footer-icons" fontSize="small" />
+              Contributors
+            </span>
           </Link>
         </li>
         <li>
           <Link to="/dummygram/help-center" className="footer-link">
-            <span style={{display: "flex", gap: "6px"}}><HelpIcon className="footer-icons" fontSize="small" />Help-center</span>
+            <span style={{ display: "flex", gap: "6px" }}>
+              <HelpIcon className="footer-icons" fontSize="small" />
+              Help-center
+            </span>
           </Link>
         </li>
         <li>
           <Link to="/dummygram/guidelines" className="footer-link">
-            <span style={{display: "flex", gap: "6px"}}><GuidelinesIcon className="footer-icons" fontSize="small" />Guidelines</span>
+            <span style={{ display: "flex", gap: "6px" }}>
+              <GuidelinesIcon className="footer-icons" fontSize="small" />
+              Guidelines
+            </span>
           </Link>
         </li>
       </ul>
