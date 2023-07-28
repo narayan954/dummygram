6fef9ed330ca70413aa05bcddc0c3364129b502d
Anamika Yadav <aanyayadav419@gmail.com>
2023-07-29 00:34:32 +0530

    moved search to navbar and displayed in a modal (#1022)
    Co-authored-by: Narayan soni <narayansoni854@gmail.com>

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,6 +1,6 @@
 import "./index.css";
 
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 import { AiOutlineInsertRowAbove } from "react-icons/ai";
 import ChatIcon from "@mui/icons-material/Chat";
@@ -8,9 +8,68 @@ import { Darkmode } from "../../reusableComponents";
 import { Logo } from "../../reusableComponents";
 import { auth } from "../../lib/firebase";
 import { useNavigate } from "react-router-dom";
+import SearchIcon from "@mui/icons-material/Search";
+import CloseIcon from "@mui/icons-material/Close";
+import { FaSearch } from "react-icons/fa";
+import blankImg from "../../assets/blank-profile.webp";
+import { db } from "../../lib/firebase";
+import Modal from "@mui/material/Modal";
+import { Box } from "@mui/material";
+const PAGESIZE = 7;
 
 function Navbar({ onClick, user, setUser }) {
   const navigate = useNavigate();
+  const [open, setOpen] = useState(false);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+  const [debouncedQuery, setDebouncedQuery] = useState("");
+
+  const handlequery = () => {
+    setSearchQuery("");
+  };
+  const handleSearchModal = () => {
+    setOpen(!open);
+  };
+
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      setDebouncedQuery(searchQuery);
+    }, 2000);
+
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, [searchQuery]);
+
+  useEffect(() => {
+    // Call fetchUsers function whenever debouncedQuery changes after the 2-second delay
+    fetchUsers();
+  }, [debouncedQuery]);
+
+  const handleSearch = (e) => {
+    setSearchQuery(e.target.value);
+  };
+
+  const fetchUsers = async () => {
+    const value = searchQuery;
+    if (value.length > 0) {
+      const usersCollection = await db
+        .collection("users")
+        .orderBy("name", "asc")
+        .startAt(value.toUpperCase())
+        .endAt(value.toLowerCase())
+        .limit(PAGESIZE)
+        .get();
+
+      const fetchedUsers = usersCollection.docs.map((doc) => ({
+        id: doc.id,
+        user: doc.data(),
+      }));
+      setSearchResults(fetchedUsers);
+    } else {
+      setSearchResults([]);
+    }
+  };
 
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((authUser) => {
@@ -32,6 +91,9 @@ function Navbar({ onClick, user, setUser }) {
       <div className="app__header">
         <Logo />
         <div className="navSpace">
+          <div className="searchbar" onClick={handleSearchModal}>
+            <SearchIcon sx={{ fontSize: 30 }} className="icon" />
+          </div>
           <div className="container">
             <div className="rowConvert" onClick={onClick}>
               <AiOutlineInsertRowAbove style={{ margin: "auto" }} size={30} />
@@ -45,6 +107,117 @@ function Navbar({ onClick, user, setUser }) {
             </div>
           </div>
           <Darkmode themeClass="themeButton" />
+          <Modal
+            open={open}
+            onClose={handleSearchModal}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+          >
+            <Box
+              sx={{
+                position: "relative",
+                top: "50%",
+                left: "50%",
+                transform: "translate(-50%, -50%)",
+                height: "80vh",
+                boxShadow: 24,
+                backdropFilter: "blur(7px)",
+                border: "1px solid #fff",
+                zIndex: "1000",
+                textAlign: "center",
+                borderRadius: "5%",
+                position: "relative",
+              }}
+              className="search-modal"
+            >
+              <div className="search-closeicon" onClick={handleSearchModal}>
+                <CloseIcon sx={{ fontSize: 40 }} />
+              </div>
+              <div
+                style={{
+                  position: "absolute",
+                  top: "50px",
+                  width: "100%",
+                  height: "90%",
+                }}
+              >
+                <div className="search-bar">
+                  <input
+                    type="search"
+                    className="search-input"
+                    value={searchQuery}
+                    placeholder="Search users..."
+                    onChange={handleSearch}
+                  />
+                  <label className="search-icon">
+                    <FaSearch />
+                  </label>
+                  {!searchQuery ? (
+                    ""
+                  ) : (
+                    <span
+                      style={{
+                        position: "absolute",
+                        right: "57px",
+                        display: "flex",
+                        color: "rgba(0, 0, 0, 0.8)",
+                        cursor: "pointer",
+                      }}
+                      onClick={handlequery}
+                    >
+                      <CloseIcon sx={{ fontSize: "30" }} />
+                    </span>
+                  )}
+                </div>
+                {searchResults.length > 0 ? (
+                  <section className="searched-user-container">
+                    <ul className="searched-user-sub-container">
+                      {searchResults.map(({ id, user }) => {
+                        return (
+                          <li
+                            key={id}
+                            className="searched-user-li"
+                            onClick={() =>
+                              navigate(`/dummygram/user/${user.username}`)
+                            }
+                          >
+                            <img
+                              src={user?.photoURL ? user.photoURL : blankImg}
+                              alt={user.name}
+                              className="searched-user-avatar"
+                            />
+                            <span
+                              style={{
+                                display: "flex",
+                                flexDirection: "column",
+                                alignItems: "flex-start",
+                              }}
+                            >
+                              <h5 className="searched-user-name">
+                                {user.name}
+                              </h5>
+                              <p className="searched-user-username">
+                                @{user.username}
+                              </p>
+                            </span>
+                          </li>
+                        );
+                      })}
+                    </ul>
+                  </section>
+                ) : (
+                  <Box>
+                    <div
+                      style={{ marginTop: "5px", marginBottom: "1.5rem" }}
+                      align="center"
+                    >
+                      <div className="text-white">Nothing to search</div>
+                    </div>
+                  </Box>
+                )}
+              </div>
+            </Box>
+          </Modal>
         </div>
       </div>
     )
