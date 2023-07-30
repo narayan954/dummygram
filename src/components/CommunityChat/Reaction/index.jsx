ece0054cc4e4e0d0c22807ef6182ab23b7c40947
Palak <Palakgoyal0304@gmail.com>
2023-07-31 04:54:01 +0530

    Chat reaction (#1048)
    * Add chat reaction
    
    * style: format
    
    ---------
    
    Co-authored-by: narayan soni <narayansoni854@gmail.com>

---
+++ src/components/CommunityChat/Reaction/index.jsx
@@ -0,0 +1,93 @@
+import "./index.css";
+
+import AddReactionIcon from "@mui/icons-material/AddReaction";
+import { db } from "../../../lib/firebase";
+import { useSnackbar } from "notistack";
+import { useState } from "react";
+
+const Reaction = ({ message, userUid }) => {
+  const [reactionOpen, setReactionOpen] = useState(false);
+  const { id, reaction } = message;
+  const { enqueueSnackbar } = useSnackbar();
+
+  function checkOccurenceOfReaction() {
+    let reactionsArr = Object.keys(reaction);
+    let includess = null;
+
+    for (const rxn of reactionsArr) {
+      if (reaction[rxn].includes(userUid)) {
+        includess = rxn;
+        break;
+      }
+    }
+
+    return includess;
+  }
+
+  async function addReaction(type) {
+    let updatedData = reaction;
+    const msgDocRef = db.collection("messages").doc(id);
+
+    if (!updatedData) {
+      updatedData = {
+        [type]: [userUid],
+      };
+    } else {
+      const rxnUser = checkOccurenceOfReaction();
+      if (rxnUser) {
+        const rxnIdx = reaction[rxnUser].indexOf(userUid);
+        updatedData[rxnUser].splice(rxnIdx, 1);
+        if (rxnUser !== type) {
+          if (updatedData[type]) {
+            updatedData[type].push(userUid);
+          } else {
+            updatedData = {
+              ...updatedData,
+              [rxnUser]: [userUid],
+            };
+          }
+        }
+      } else {
+        if (updatedData[type]) {
+          updatedData[type].push(userUid);
+        } else {
+          updatedData = {
+            ...updatedData,
+            [type]: [userUid],
+          };
+        }
+      }
+    }
+
+    msgDocRef
+      .update({
+        reaction: updatedData,
+      })
+      .catch((e) => {
+        enqueueSnackbar("Error while reacting", {
+          variant: "error",
+        });
+      });
+
+    setReactionOpen(false);
+  }
+
+  return (
+    <div>
+      <AddReactionIcon
+        className="msg-reaction-icon"
+        onClick={() => setReactionOpen((prev) => !prev)}
+      />
+      {reactionOpen && (
+        <span className="msg-reaction-container">
+          <p onClick={() => addReaction("smiley")}>😅</p>
+          <p onClick={() => addReaction("like")}>❤️</p>
+          <p onClick={() => addReaction("laughing")}>😂</p>
+          <p onClick={() => addReaction("thumbsUp")}>👍</p>
+        </span>
+      )}
+    </div>
+  );
+};
+
+export default Reaction;
