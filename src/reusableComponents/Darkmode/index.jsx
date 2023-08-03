5e0e8becda7706c0abb1649201da9f3637c3f3f8
Palak <Palakgoyal0304@gmail.com>
2023-08-04 03:11:10 +0530

    Change theme toggle button to switch (#1110)
    * Change theme toggle button to switch
    
    * style: format
    
    ---------
    
    Co-authored-by: Narayan soni <narayansoni854@gmail.com>

--- src/reusableComponents/Darkmode/index.jsx
+++ src/reusableComponents/Darkmode/index.jsx
@@ -1,14 +1,15 @@
 import "./index.css";
 
 import React, { useState } from "react";
-import { playLightOnSound, playLightOffSound } from "../../js/sounds";
+import { playLightOffSound, playLightOnSound } from "../../js/sounds";
 
-import { DarkMode } from "@mui/icons-material";
-import { LightMode } from "@mui/icons-material";
+import CustomizedSwitch from "./ToggleSwitch";
 
-const Darkmode = ({ themeClass }) => {
-  const [modeStatus, setModeStatus] = useState(false);
+const Darkmode = () => {
   let darkMode = localStorage.getItem("darkMode");
+  const [modeStatus, setModeStatus] = useState(
+    darkMode == "enabled" ? false : true,
+  );
 
   function enableDarkMode() {
     //add the class darkmode to the body
@@ -44,21 +45,10 @@ const Darkmode = ({ themeClass }) => {
     }
   };
 
-  const styles = {
-    activity: { height: 30, width: 40, color: "#0516cbb0" },
-    disable: { height: 30, width: 40, color: "#d8860b" },
-  };
-
   return (
     <div>
-      <button onClick={darkModeToggle} className={themeClass}>
-        <span>
-          {modeStatus == false ? (
-            <LightMode style={styles.disable} />
-          ) : (
-            <DarkMode style={styles.activity} />
-          )}
-        </span>
+      <button onClick={darkModeToggle} className="theme-toggle-btn">
+        <CustomizedSwitch modeStatus={modeStatus} />
       </button>
     </div>
   );
