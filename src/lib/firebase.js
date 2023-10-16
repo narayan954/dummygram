f1f04c2e22db00af3d149abe9f00da979deb2195
Restyled.io <commits@restyled.io>
2023-10-16 14:58:57 +0000

    Restyled by prettier

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -3,15 +3,15 @@ import "firebase/compat/firestore";
 import "firebase/compat/storage";
 
 import firebase from "firebase/compat/app";
-import {v4 as uuid} from "uuid";
+import { v4 as uuid } from "uuid";
 
 const firebaseApp = firebase.initializeApp({
-  apiKey : import.meta.env.VITE_DUMMYGRAM_APIKEY,
-  authDomain : import.meta.env.VITE_DUMMYGRAM_AUTHDOMAIN,
-  projectId : import.meta.env.VITE_DUMMYGRAM_PROJECTID,
-  storageBucket : import.meta.env.VITE_DUMMYGRAM_STORAGEBUCKET,
-  messagingSenderId : import.meta.env.VITE_DUMMYGRAM_MESSAGINGSENDERID,
-  appId : import.meta.env.VITE_DUMMYGRAM_APPID,
+  apiKey: import.meta.env.VITE_DUMMYGRAM_APIKEY,
+  authDomain: import.meta.env.VITE_DUMMYGRAM_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_DUMMYGRAM_PROJECTID,
+  storageBucket: import.meta.env.VITE_DUMMYGRAM_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_DUMMYGRAM_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_DUMMYGRAM_APPID,
 });
 
 // Use these for db & auth
@@ -30,16 +30,16 @@ const storage = firebase.storage();
 
 function handleMultiUpload(files, options = {}) {
   const _options = Object.assign(
-      {
-        storageLocation : "images",
-        /**
-         *
-         * @param {number} _percentage
-         */
-        onUploadProgress : (_percentage) => {},
-        generateThumbnails : false,
-      },
-      options,
+    {
+      storageLocation: "images",
+      /**
+       *
+       * @param {number} _percentage
+       */
+      onUploadProgress: (_percentage) => {},
+      generateThumbnails: false,
+    },
+    options,
   );
 
   let totalSize = 0;
@@ -48,8 +48,9 @@ function handleMultiUpload(files, options = {}) {
   const uploadPromises = files.map((file) => {
     const fileName = uuid() + "." + file.name.split(".").pop();
     return new Promise((resolve, reject) => {
-      const task =
-          storage.ref(`${_options.storageLocation}/${fileName}`).put(file);
+      const task = storage
+        .ref(`${_options.storageLocation}/${fileName}`)
+        .put(file);
 
       /** @type {null|number} */
       let currUploadTotalSize = null;
@@ -57,85 +58,93 @@ function handleMultiUpload(files, options = {}) {
       let lastUploadedSize = null;
 
       task.on(
-          "state_changed",
-          (snapshot) => {
-            if (currUploadTotalSize === null) {
-              totalSize += snapshot.totalBytes;
-              currUploadTotalSize = snapshot.totalBytes;
-            }
+        "state_changed",
+        (snapshot) => {
+          if (currUploadTotalSize === null) {
+            totalSize += snapshot.totalBytes;
+            currUploadTotalSize = snapshot.totalBytes;
+          }
 
-            totalUploaded -= lastUploadedSize ?? 0;
-            totalUploaded += snapshot.bytesTransferred;
+          totalUploaded -= lastUploadedSize ?? 0;
+          totalUploaded += snapshot.bytesTransferred;
 
-            lastUploadedSize = snapshot.bytesTransferred;
+          lastUploadedSize = snapshot.bytesTransferred;
 
-            _options.onUploadProgress(
-                Math.round((totalUploaded / totalSize) * 100),
-            );
-          },
-          (error) => {
-            if (currUploadTotalSize !== null) {
-              totalSize -= currUploadTotalSize;
-            }
+          _options.onUploadProgress(
+            Math.round((totalUploaded / totalSize) * 100),
+          );
+        },
+        (error) => {
+          if (currUploadTotalSize !== null) {
+            totalSize -= currUploadTotalSize;
+          }
 
-            if (lastUploadedSize !== null) {
-              totalUploaded -= lastUploadedSize;
-            }
+          if (lastUploadedSize !== null) {
+            totalUploaded -= lastUploadedSize;
+          }
 
-            reject(error);
-          },
-          () => {
-            storage.ref(_options.storageLocation)
-                .child(fileName)
-                .getDownloadURL()
-                .then((url) => {
-                  if (_options.generateThumbnails) {
-                    const thumbnailScale = 1 / 10;
+          reject(error);
+        },
+        () => {
+          storage
+            .ref(_options.storageLocation)
+            .child(fileName)
+            .getDownloadURL()
+            .then((url) => {
+              if (_options.generateThumbnails) {
+                const thumbnailScale = 1 / 10;
 
-                    const image = new Image();
-                    image.src = URL.createObjectURL(file);
+                const image = new Image();
+                image.src = URL.createObjectURL(file);
 
-                    image.addEventListener("load", () => {
-                      const canvas = document.createElement("canvas");
-                      canvas.width = image.naturalWidth * thumbnailScale;
-                      canvas.height = image.naturalHeight * thumbnailScale;
+                image.addEventListener("load", () => {
+                  const canvas = document.createElement("canvas");
+                  canvas.width = image.naturalWidth * thumbnailScale;
+                  canvas.height = image.naturalHeight * thumbnailScale;
 
-                      canvas.getContext("2d").drawImage(
-                          image, 0, 0, canvas.width, canvas.height);
+                  canvas
+                    .getContext("2d")
+                    .drawImage(image, 0, 0, canvas.width, canvas.height);
 
-                      resolve({
-                        thumbnail : canvas.toDataURL(),
-                        imageWidth : image.naturalWidth,
-                        imageHeight : image.naturalHeight,
-                        imageUrl : url,
-                      });
-                    });
+                  resolve({
+                    thumbnail: canvas.toDataURL(),
+                    imageWidth: image.naturalWidth,
+                    imageHeight: image.naturalHeight,
+                    imageUrl: url,
+                  });
+                });
 
-                    image.addEventListener("error", () => {
-                      resolve({
-                        imageUrl : url,
-                        thumbnail : null,
-                        imageWidth : 0,
-                        imageHeight : 0,
-                      });
-                    });
-                  } else {
-                    resolve(url);
-                  }
-                })
-                .catch((error) => { reject(error); });
-          },
+                image.addEventListener("error", () => {
+                  resolve({
+                    imageUrl: url,
+                    thumbnail: null,
+                    imageWidth: 0,
+                    imageHeight: 0,
+                  });
+                });
+              } else {
+                resolve(url);
+              }
+            })
+            .catch((error) => {
+              reject(error);
+            });
+        },
       );
     });
   });
 
   return new Promise((resolve, reject) => {
     Promise.all(uploadPromises)
-        .then((uploads) => { resolve(uploads); })
-        .catch((error) => { reject(error); });
+      .then((uploads) => {
+        resolve(uploads);
+      })
+      .catch((error) => {
+        reject(error);
+      });
   });
 }
 
-export {db, auth, storage, handleMultiUpload};
+export { db, auth, storage, handleMultiUpload };
 export const googleProvider = new firebase.auth.GoogleAuthProvider();
 export const facebookProvider = new firebase.auth.FacebookAuthProvider();
