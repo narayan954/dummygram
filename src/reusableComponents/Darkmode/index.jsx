2a05c4d5df6a6f822d8803ee8dbee3d3e7a3246d
Akshat <82810411+Afterdie@users.noreply.github.com>
2023-12-08 17:21:26 +0530

    fixed positioning

--- src/reusableComponents/Darkmode/index.jsx
+++ src/reusableComponents/Darkmode/index.jsx
@@ -44,7 +44,7 @@ const Darkmode = () => {
   };
 
   return (
-    <div>
+    <div className="toggle-container">
       <button onClick={darkModeToggle} className="theme-toggle-btn">
         <CustomizedSwitch modeStatus={modeStatus} />
       </button>
